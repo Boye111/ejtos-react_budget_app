0a9025cb0fad3d132997849d3636951ca05880fb
Boye <adeboyeakingbelure18@yahoo.com>
2023-01-19 10:52:27 -0500

    budget allocation

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,13 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import ViewBudget from './viewbudget';
+import EditBudget from './EditBudget';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+    const [Editbudget, setEditBudget] = useState(false);
+
+    const handleEditClick = () => {
+        setEditBudget(true);
+    };
+
+    const handleSaveClick = (value) => {
+
+        if(value > 20000) {
+            alert("Budget cannot exceed 20000");
+            setEditBudget("");
+            return;
+        }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: value,
+        });
+        setEditBudget(false);
+    };
 
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+        <div class='alert alert-secondary p-3 d-flex align-items-center justify-content-between'>
+            {Editbudget ? (
+                <EditBudget handleSaveClick={handleSaveClick} budget={budget} />
+            ) : (
+                <ViewBudget handleEditClick={handleEditClick} budget={budget} />
+            )}
         </div>
+
     );
 };
 
