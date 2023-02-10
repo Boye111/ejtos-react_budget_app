2962d301dd1e6a62ef171bd83210ffbf839ea0d1
Boye <adeboyeakingbelure18@yahoo.com>
2023-02-10 10:05:46 -0500

    react budget app

---
+++ src/components/Currency.js
@@ -0,0 +1,61 @@
+import React, {useContext, useState, useEffect} from 'react';
+import {AppContext} from '../context/AppContext';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle';
+
+const Currency = () => {
+    const {currency, dispatch} = useContext(AppContext);
+    const [currencystate, setCurrencystate] = useState(currency);
+
+    const updateCurrencyicon = () => {
+        switch(currency) {
+            case '$':
+                setCurrencystate('$ Dollars');
+                break;
+            case '£':
+                setCurrencystate('£ Pound');
+                break;
+            case '€':
+                setCurrencystate('€ Euro');
+                break;
+            case '₹':
+                setCurrencystate('₹ Ruppee');
+                break;
+            default:
+                setCurrencystate('£ Pound');
+
+        }
+    };
+
+    useEffect(() => {
+        updateCurrencyicon()
+    });
+
+    const currencychange = (newCurrency) => {
+        if (['$', '£', '€', '₹'].includes(newCurrency)) {
+            console.log(newCurrency);
+            dispatch({
+                type: 'CHG_CURRENCY',
+                payload: newCurrency
+            });
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <Dropdown>
+                <Dropdown.Toggle className='dropdown' variant='success' id='dropdown-basic'>
+                    Currency {currencystate}
+                </Dropdown.Toggle>
+                <Dropdown.Menu className='menu'>
+                    <Dropdown.Item onClick={() => {currencychange('$')}} className='currency-item' href="#/action-1">$ Dollar</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {currencychange('£')}} className='currency-item' href="#/action-2">£ Pound</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {currencychange('€')}} className='currency-item' href="#/action-3">€ Euro</Dropdown.Item>
+                    <Dropdown.Item onClick={() => {currencychange('₹')}} className='currency-item' href="#/action-4">₹ Ruppee</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    );
+}
+
+export default Currency;
--- src/components/EditBudget.js
+++ src/components/EditBudget.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const EditBudget = (props) => {
     const [value, setValue] = useState(props.budget);
+    const [ currency ] = useContext(AppContext);
     return (
         <>
+        <div className='input-group-prepend'>
+        
+        </div>
         <input
             required='required'
             type='number'
+            step='10'
             class='form-control mr-3'
             id='name'
             value={value}
